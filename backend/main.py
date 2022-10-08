dde6b27f875c64df6ae94c8976af30f7478fce62
Yannick <33485945+YannickAaron@users.noreply.github.com>
2022-10-08 09:27:01 +0100

    celery working example

--- backend/main.py
+++ backend/main.py
@@ -1,64 +1,25 @@
-from typing import List
+import uvicorn as uvicorn
+from fastapi import FastAPI
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime
-import json
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # can alter with time
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+from pythonxreact.config.celery_utils import create_celery
+from pythonxreact.routers import universities
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+def create_app() -> FastAPI:
+    current_app = FastAPI(
+        title="Asynchronous tasks processing with Celery and RabbitMQ",
+        description="Sample FastAPI Application to demonstrate Event " "driven architecture with Celery and RabbitMQ",
+        version="1.0.0",
+    )
+
+    current_app.celery_app = create_celery()  # create celery app instance
+    current_app.include_router(universities.router)
+    return current_app
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+app = create_app()
+celery = app.celery_app
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=9000, reload=True)
---
+++ backend/pythonxreact/celery_tasks/tasks.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from celery import shared_task
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={"max_retries": 5},
+    name="universities:get_all_universities_task",
+)
+def get_all_universities_task(self, countries: List[str]):
+    data: dict = {}
+    data = {"countries": countries}
+    return data
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={"max_retries": 5},
+    name="university:get_university_task",
+)
+def get_university_task(self, country: str):
+    data = {"country": country}
+    return data
