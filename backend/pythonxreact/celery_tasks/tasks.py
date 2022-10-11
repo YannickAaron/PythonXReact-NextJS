4dfe86664ee7ad432797d3ee9da7065898d09413
Yannick <33485945+YannickAaron@users.noreply.github.com>
2022-10-11 15:16:50 +0100

    more

--- backend/pythonxreact/celery_tasks/tasks.py
+++ backend/pythonxreact/celery_tasks/tasks.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from celery import shared_task
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 
 
 @shared_task(
@@ -36,5 +38,17 @@ def get_university_task(self, country: str):
     name="university:get_university_task",
 )
 def get_university_task(self, country: str):
+    data = {"country": country}
+    return data
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={"max_retries": 5},
+    name="test:count_to_X",
+)
+def get_university_task(self, X: int):
     data = {"country": country}
     return data
